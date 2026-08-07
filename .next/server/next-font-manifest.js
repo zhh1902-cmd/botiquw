0de0bd08310a8788ebc811e193b292cdd7768acc
zhh1902-cmd <zhh1902@gmail.com>
2026-08-07 06:41:27 +0530

    Updated build-manifest.json

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/7b89a4fd5e90ede0-s.p.woff2\",\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/eaead17c7dbfcd5d-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
